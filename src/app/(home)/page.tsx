97d45e931e951883cf9d47dd098059a2c8f23eaa
Angga Pratama <aa.angga.pratama@gmail.com>
2025-04-02 01:27:27 +0700

    customize part 1

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </main>
-  );
+		<main className="flex flex-1 flex-col justify-center text-center">
+			<h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+			<p className="text-fd-muted-foreground">
+				You can open{" "}
+				<Link
+					href="/docs/1.x"
+					className="text-fd-foreground font-semibold underline"
+				>
+					/docs
+				</Link>{" "}
+				and see the documentation.
+			</p>
+		</main>
+	);
 }
--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,53 +1,59 @@
-import { source } from '@/lib/source';
+import { source } from "@/lib/source";
 import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
-import defaultMdxComponents, { createRelativeLink } from 'fumadocs-ui/mdx';
+	DocsPage,
+	DocsBody,
+	DocsDescription,
+	DocsTitle,
+} from "fumadocs-ui/page";
+import { notFound } from "next/navigation";
+import defaultMdxComponents, { createRelativeLink } from "fumadocs-ui/mdx";
+import { getTableOfContents } from "fumadocs-core/server";
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export default async function Page(props: { params: Promise<{ slug?: string[] }> }) {
+	const params = await props.params;
+	const page = source.getPage(params.slug);
+	if (!page) notFound();
 
-  const MDXContent = page.data.body;
-
-  return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
-        <MDXContent
-          components={{
-            ...defaultMdxComponents,
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-            // you can add other MDX components here
-          }}
-        />
-      </DocsBody>
-    </DocsPage>
-  );
+	const MDXContent = page.data.body;
+	console.log(page);
+	return (
+		<DocsPage
+			toc={page.data.toc}
+			full={page.data.full}
+			tableOfContent={{
+				style: "clerk",
+			}}
+			lastUpdate={page.data.lastModified}
+		>
+			<DocsTitle>{page.data.title}</DocsTitle>
+			<DocsDescription>{page.data.description}</DocsDescription>
+			<DocsBody>
+				<MDXContent
+					components={{
+						...defaultMdxComponents,
+						// this allows you to link to other pages with relative file paths
+						a: createRelativeLink(source, page),
+						// you can add other MDX components here
+					}}
+				/>
+			</DocsBody>
+		</DocsPage>
+	);
 }
 
 export async function generateStaticParams() {
-  return source.generateParams();
+	return source.generateParams();
 }
 
 export async function generateMetadata(props: {
-  params: Promise<{ slug?: string[] }>;
+	params: Promise<{ slug?: string[] }>;
 }) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+	const params = await props.params;
+	const page = source.getPage(params.slug);
+	if (!page) notFound();
 
-  return {
-    title: page.data.title,
-    description: page.data.description,
-  };
+	return {
+		title: page.data.title,
+		description: page.data.description,
+	};
 }
