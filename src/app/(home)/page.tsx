492b050ae7a5b62fc44803bdae0a01fe6a2b0bc1
Angga Pratama <aa.angga.pratama@gmail.com>
2025-04-07 22:07:46 +0700

    update home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,10 +2,12 @@ import { Building2, Github } from "lucide-react";
 import Image from "next/image";
 import Link, { LinkProps } from "next/link";
 import Spot from "public/spot.png";
+import QuickInstallScreenshot from "public/themes/ocean.png";
+import FumadocsSimpleStarter from "public/fumadocs-simple-starter.png";
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-1 flex-col justify-center text-center">
+		<main className="flex flex-1 flex-col justify-center text-center py-8 md:py-16">
 			<div className="absolute inset-0 z-[-1] overflow-hidden duration-1000 animate-in fade-in [perspective:2000px]">
 				<div
 					className="absolute bottom-[20%] left-1/2 size-[1200px] origin-bottom bg-fd-primary/30 opacity-30"
@@ -25,9 +27,11 @@ export default function HomePage() {
 					priority
 				/>
 			</div>
-			<h1 className="mb-4 text-2xl font-bold">
-				This is The Home Page of My App Docs Website
-			</h1>
+			<h1 className="mb-4 text-2xl font-bold">What is this?</h1>
+			<p className="mb-4 text-fd-muted-foreground">
+				This is a documentation or knowledge base website created using
+				Fumadocs.
+			</p>
 			<p className="mb-8 text-fd-muted-foreground">
 				You can open{" "}
 				<Link
@@ -36,15 +40,54 @@ export default function HomePage() {
 				>
 					/docs
 				</Link>{" "}
-				and see the documentation.
+				and see the exmaple of the documentation.
 			</p>
-			<h2 className="mb-4 text-2xl font-bold">If you're using Fumadocs CLI</h2>
-			<p className="mb-8 text-fd-muted-foreground">you will only get...</p>
+			<h2 className="mb-4 text-2xl font-bold">Why creating this?</h2>
+			<p className="mb-8 text-fd-muted-foreground">
+				This is a demo to show you the power of Fumadocs in creating
+				documentation website
+			</p>
+			<p className="mb-8 text-fd-muted-foreground">
+				You can go to fumadocs's docs and try it's quick-start{" "}
+				<span>
+					<pre>`npm create fumadocs-app`</pre>
+				</span>{" "}
+				command.
+			</p>
+			<p className="mb-8 text-fd-muted-foreground">but, you will only get...</p>
+			<Image
+				alt="spot"
+				src={FumadocsSimpleStarter}
+				sizes="100vw"
+				className="rounded-xl mx-auto size-full min-w-[800px] max-w-7xl border-8 border-fd-primary"
+				priority
+			/>
+			<p className="mt-8 mb-2 text-fd-muted-foreground">
+				I know you want something like this:
+			</p>
+			<p className="mb-2 text-fd-muted-foreground">
+				~~~like on the fumadocs's docs~~~
+			</p>
+			<Image
+				alt="spot"
+				src={QuickInstallScreenshot}
+				sizes="100vw"
+				className="mx-auto size-full min-w-[800px] max-w-fd-container"
+				priority
+			/>
 			<p className="mb-4 text-fd-muted-foreground">
-				That's why I created this starter. Get it here:
+				That's why I created this starter, <br></br>to skip all the setup,{" "}
+				<br></br>unlocking all of the Fumadocs power from the very begining,{" "}
+				<br></br>and focus just to the content.
+			</p>
+			<p className="mb-4 text-fd-muted-foreground">
+				Most of the contents are from fumadocs repository though.
+			</p>
+			<p className="mb-4 text-fd-muted-foreground">
+				and you can have it too, get it here:
 			</p>
 			<div className="mb-8 mx-auto w-2xl grid grid-cols-1 gap-4 text-left">
-				<Item href="https://github.com/fuma-nama/fumadocs">
+				<Item href="https://github.com/anggastudio/fumadocs-contentful-starter">
 					<div className="flex items-center gap-4">
 						<Icon>
 							<Github className="size-full" />
@@ -56,12 +99,13 @@ export default function HomePage() {
 
 					<p className="text-sm text-fd-muted-foreground">
 						The fumadocs starter, but with content. It is easier to delete
-						unused content than to create it from scratch. Menyala abangkuuh.
+						unused content than to create it from scratch or to setup from the
+						begining. Menyala abangkuuh.
 					</p>
 				</Item>
 			</div>
 			<p className="mb-4 text-fd-muted-foreground">
-				Although I'd be happier if Fuma could create a Contentful starter
+				Although I'd be happier if Fuma could create a contentful starter
 				himself.
 			</p>
 			<p className="text-fd-muted-foreground">
@@ -103,6 +147,7 @@ function Item(
 	return (
 		<Link
 			{...props}
+			target="_blank"
 			className="rounded-2xl border border-transparent p-6 shadow-lg"
 			style={{
 				backgroundImage:
